aa43c550a61c92511acb46f0582274a804e6dce4
NeptuneG <goss.beta@gmail.com>
2017-07-27 16:09:57 +0800

    add eureka

---
+++ microservice-discovery-eureka/src/main/java/com/dumb/microservice/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.dumb.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by goss.beta on 2017/7/27.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
