27de05554cbbf89a3919fef10be2d8af4fa44936
Guan Yu <goss.beta@gmail.com>
2017-07-31 18:41:15 +0800

    add hystrix dependency to ribbon

--- microservice-ribbon/src/main/java/com/dumb/microservice/service/RibbonService.java
+++ microservice-ribbon/src/main/java/com/dumb/microservice/service/RibbonService.java
@@ -1,5 +1,8 @@
 package com.dumb.microservice.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,12 +14,19 @@ import org.springframework.web.client.RestTemplate;
 public class RibbonService {
     @Autowired
     private RestTemplate restTemplate;
+    private static final Logger LOGGER = LoggerFactory.getLogger(RibbonService.class);
 
+    @HystrixCommand(fallbackMethod = "getSongRecordsFallback")
     public String getSongRecords(String urlSuffix) {
         return this.restTemplate.getForObject("http://MICROSERVICE-SAMPLE-SERVICE/sample/" + urlSuffix, String.class);
     }
 
     public String getCallInfo() {
         return this.restTemplate.getForObject("http://MICROSERVICE-SAMPLE-SERVICE/sample/count", String.class);
+    }
+
+    public String getSongRecordsFallback(String urlSuffix, Throwable e) {
+        RibbonService.LOGGER.error("Exception when accessing https://site.douban.com/{} : {}", urlSuffix, e.getMessage());
+        return e.getMessage();
     }
 }
