e237538378fa48d6dc241d7bdc86aa63c6677c13
Guan Yu <goss.beta@gmail.com>
2017-07-31 18:41:54 +0800

    add hystrix dashboard

---
+++ microservice-hystrix-dashboard/src/main/java/com/dumb/microservice/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.dumb.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by guayu on 7/31/2017.
+ */
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
